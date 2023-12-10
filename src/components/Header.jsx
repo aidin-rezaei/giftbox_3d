b2e1a00ffb67de8647befcd12515843d9fffef67
aidin-rezaei <aydinrezaeefdr@gmail.com>
2023-12-10 12:32:33 -0800

    update Header.jsx and Second.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,14 @@ const Header = () => {
     return (
         <section className="w-full h-[calc(100vh_-_45px)] bg-[#ffe7e7] grid grid-cols-2 items-center justify-center">
             <div className="w-full h-full flex justify-center items-center flex-col gap-10">
-                <h1 className="pacifico text-7xl text-[#ff8a8a]">Register and <br /> get a gift</h1>
-                <p className="amatic text-4xl font-bold">Have a chance in <br /> this lottery by registering on this page</p>
+                <div className=" h-full flex justify-center items-start flex-col gap-10">
 
+                    <h1 className="pacifico text-7xl text-[#ff8a8a]">Register and <br /> get a gift</h1>
+                    <p className="amatic text-4xl font-bold">Have a chance in <br /> this lottery by registering on this page</p>
+                    <div>
+                        <a className="p-2 bg-[#ff8a8a] rounded-md text-white" href="">Learn more</a>
+                    </div>
+                </div>
             </div>
             <div>
             </div>
--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -1,12 +1,17 @@
 const Second = () => {
     return (
         <section className="w-full h-screen bg-white grid grid-cols-2 items-center justify-center">
-            <div className="w-full h-full flex justify-center items-center flex-col gap-10">
-                <h1 className="pacifico text-7xl text-[#ff8a8a]">Register and <br /> get a gift</h1>
-                <p className="amatic text-4xl font-bold">Have a chance in <br /> this lottery by registering on this page</p>
-
-            </div>
             <div>
+            </div>
+            <div className="w-full h-full flex justify-center items-center flex-col gap-10">
+                <div className=" h-full flex justify-center items-start flex-col gap-10">
+
+                    <h1 className="pacifico text-7xl text-[#ff8a8a]">50 gifts <br /> worth $50,000</h1>
+                    <p className="amatic text-4xl font-bold ">Maybe this is your lucky person</p>
+                    <div>
+                        <a className="p-2 bg-[#ff8a8a] rounded-md text-white" href="">Learn more</a>
+                    </div>
+                </div>
             </div>
         </section>
     );
