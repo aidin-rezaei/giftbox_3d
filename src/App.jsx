8e38043d92c74435fdacbec66e920110af27582a
aidin-rezaei <aydinrezaeefdr@gmail.com>
2023-12-10 10:38:36 -0800

    feat: add section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Second from "./components/second";
 
 function App() {
 
   return (
     <div className="App">
       <Nav/>
+      <Header/>
+      <Second/>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,12 @@ import Search from '../assets/images/search.svg';
 import Store from '../assets/images/store.svg';
 const Nav = () => {
     return (
-        <nav className="nav-wrapper">
-            <div className="nav-content">
-                <ul className="list-styled">
-                    <li>
-                        <a className="link-styled" href="">LOGO</a>
-                    </li>
+        <nav className="nav-wrapper w-full h-[45px] bg-white">
+            <div className="nav-content flex w-full h-full justify-between items-center p-5">
+                <div>
+                    <a className="link-styled font-bold" href="">LOGO</a>
+                </div>
+                <ul className="list-styled flex gap-2 justify-start items-center ">
                     <li>
                         <a className="link-styled" href="">LOGO</a>
                     </li>
