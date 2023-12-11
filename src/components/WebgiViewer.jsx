96c884db9ec2b558ee2796089f8a3c1d5e85e602
aidin-rezaei <aydinrezaeefdr@gmail.com>
2023-12-11 06:28:11 -0800

    update WebgiViewer.jsx

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -15,6 +15,7 @@ import {
 } from "webgi";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger)
 const WebgiViewer = () => {
     const canvasRef = useRef(null)
 
@@ -32,7 +33,7 @@ const WebgiViewer = () => {
         // Add plugins individually.
         await viewer.addPlugin(GBufferPlugin)
         await viewer.addPlugin(new ProgressivePlugin(32))
-        await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+        await viewer.addPlugin(new TonemapPlugin(true))
         await viewer.addPlugin(GammaCorrectionPlugin)
         await viewer.addPlugin(SSRPlugin)
         await viewer.addPlugin(SSAOPlugin)
@@ -48,7 +49,7 @@ const WebgiViewer = () => {
 
         window.scrollTo(0, 0)
 
-        let needsUpdate = ture
+        let needsUpdate = true
 
         viewer.addEventListener('preFrame',()=>{
             camera.positionTargetUpdated(true)
